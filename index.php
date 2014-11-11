e9eb625147bfc905ae1e1d5c57df61534ae5f6fc
aspang <apang@uoguelph.ca>
2014-11-11 16:42:59 -0500

    Basic structure created

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Home Page</title>
+    
+    <!-- CSS files -->
+    <link rel="stylesheet" href="./css/home.css">    
+</head>
+
+<body>
+
+Hello World!
+
+</body>
+
+</html> 
